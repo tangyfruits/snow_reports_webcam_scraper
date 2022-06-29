78da9d4150c68cf178eea1449b3b020c8653558d
Donald Tang <tangyfruits@gmail.cm>
2022-06-30 11:59:05 +1200

    py3 updates

--- ruapehu.py
+++ ruapehu.py
@@ -1,39 +1,28 @@
-import urllib2
+import urllib.request
 import time
 import schedule
+import os
 
 
 def build_url(webcam):
-    url = "https://webcams.mtruapehu.com/" + webcam.lower().replace(" ", "") +"/latest.jpg?" + str(time.time())
-    print url
+    url = "https://webcams.mtruapehu.com/" + webcam.lower().replace(" ", "") + "/latest.jpg?" + str(time.time())
+    print(url)
     return url
 
-	
+
 def save_img():
-	whakapapa = ["Happy Valley","Te Heuheu Valley","Waterfall","The Pinnacles","Far West T Bar"] #["Happy Valley","Te Heuheu Valley","Waterfall","The Pinnacles","Far West T Bar","West Ridge Far West"]
-	turoa = ["Alpine Meadows","Blyth Flat","Nga Waiheke","Giant Cafe","Highnoon Top"]
+    whakapapa = ["HV From Sky Waka", "Te Heuheu Valley", "Waterfall", "The Pinnacles", "Far West T Bar"]
+    turoa = ["Alpine Meadows", "Blyth Flat", "Giant Cafe", "Highnoon Top", "Turoa Midfield"]
 
-	for w in whakapapa:
-		print w
-		url = build_url(w)
-		f = urllib2.urlopen(url)
-		data = f.read()
-		with open("whakapapa/" + w + "/" + url.split('?')[-1] + ".jpg", "wb") as imgfile:
-			imgfile.write(data)
-                time.sleep(0.1)
-	for t in turoa:
-		print t
-		url = build_url(t)
-		f = urllib2.urlopen(url)
-		data = f.read()
-		with open("turoa/" + t + "/" + url.split('?')[-1] + ".jpg", "wb") as imgfile:
-			imgfile.write(data)
-                time.sleep(0.1)
+    ruapehu = whakapapa + turoa
+    for field in ruapehu:
+        print(field)
+        os.makedirs(field, exist_ok=True)
+        url = build_url(field)
+        resp = urllib.request.urlretrieve(url, field + "/" + url.split('?')[-1] + ".jpg")
 
-	print "done"
 
 schedule.every(15).minutes.do(save_img)
-
 
 while 1:
     schedule.run_pending()
